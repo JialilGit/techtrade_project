def68624f3ac9cca95265e31f2885ab497da88ff
Jialil Andrada <jide.andrada.up@phinmaed.com>
2024-02-27 21:23:43 +0800

    verifycsrftoken update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,10 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-<<<<<<< HEAD
-        //
-=======
->>>>>>> origin/master
         "register",
         "login"
     ];
